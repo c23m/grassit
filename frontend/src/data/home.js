7f403a7549b2244341f8e418387c6643654ebc7a
c23m <3493862861@qq.com>
2026-09-03 01:43:33 +0800

    add /frontend

---
+++ frontend/src/data/home.js
@@ -0,0 +1,48 @@
+export const articles = [
+    {
+        slug: "dog",
+        title: "Dog",
+        subtitle: "Development of Grassit"
+    },
+    {
+        slug: "deswaq",
+        title: "DESWAQ",
+        subtitle: "Disappeared Entrance: Start Without Any Quests"
+    },
+    {
+        slug: "cpp",
+        title: "C++文档",
+        subtitle: "算法竞赛"
+    },
+    {
+        slug: "entity",
+        title: "实体文档",
+        subtitle: "DOG说明书"
+    },
+    {
+        slug: "entities_list",
+        title: "实体列表",
+        subtitle: "DOG说明书"
+    }
+]
+
+export const recommendations = [
+    {
+        title: "访问学校官网",
+        description: "本站可以跳转到学校官网。真是一项实用的功能！本站可以跳转到学校官网。真是一项实用的功能啊！本站可以跳转到学校官网。真是一项实用的功能啊！",
+        link: "https://www.hfut.edu.cn",
+        image: "sunny"
+    },
+    {
+        title: "请输入文本",
+        description: "会跳转到本站测试界面。一个用来测试html/css/js的界面。会跳转到本站测试界面。一个用来测试html/css/js的界面。会跳转到本站测试界面。一个用来测试html/css/js的界面。",
+        link: "/test",
+        image: "dessert"
+    },
+    {
+        title: "占位符",
+        description: "盼望着，盼望着，东风来了，春天的脚步近了。一切都像刚睡醒的样子，欣欣然张开了眼。山朗润起来了，水涨起来了，太阳的脸红起来了。小草偷偷地从土里钻出来，嫩嫩的，绿绿的。",
+        link: "https://deepseek.com",
+        image: "dark"
+    }
+]
---
+++ frontend/src/views/ArticleView.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { data, content } = defineProps({
+    data: Object,
+    content: String
+})
+
+import { ref, computed } from 'vue'
+import { marked } from 'marked'
+import BaseLayout from '@/components/layouts/BaseLayout.vue';
+
+const html = computed(() => marked.parse(content))
+</script>
+
+<template>
+    <BaseLayout>
+        <aside class="menu"></aside>
+        <article class="content" v-html="html"></article>
+    </BaseLayout>
+</template>
+
+<style scoped></style>
