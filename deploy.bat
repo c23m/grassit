ebc70901fe0ba7f894702dc591c18f923ed3d4b2
AquerKing <aquerking@163.com>
2026-09-03 19:19:56 +0800

    implement deploy scripts supporting on Linux(Debian-based ditros) and Windows

---
+++ deploy.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+
+cd .\backend
+docker buildx build . --tag grassit-backend:latest
+
+set DOCKER_COMPOSE_CMD=docker-compose
+
+docker compose version >nul 2>&1
+
+if %errorlevel% equ 0 (
+    set DOCKER_COMPOSE_CMD=docker compose
+)
+
+echo [INFO] Using commandl: !DOCKER_COMPOSE_CMD!
+
+!DOCKER_COMPOSE_CMD! up -docker
+
+endlocal
