5c5c404e0d3b7ed7a68a74e64383eddd75ccd35f
c23m <3493862861@qq.com>
2026-09-08 14:59:26 +0800

    添加前端子模块 gst-frontend

--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import 'github-markdown-css/github-markdown.css'
-
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
-
-createApp(App).use(router).mount('#app')
